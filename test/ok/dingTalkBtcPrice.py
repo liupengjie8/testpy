1bd00677fcb15dc2a4621bee66f599f13d7e4f8c
liupengjie <liupengjie123>
2021-06-02 16:57:18 +0800

    dingding btc_pirce push

--- test/ok/dingTalkBtcPrice.py
+++ test/ok/dingTalkBtcPrice.py
@@ -1,5 +1,6 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import json
 import time
 from dingtalkchatbot.chatbot import DingtalkChatbot
 # WebHook地址
@@ -7,8 +8,7 @@ webhook = 'https://oapi.dingtalk.com/robot/send?access_token=4fb919b05bde12a70d7
 secret = 'SECf50a9ff48267e2669b9402ad8a637f9219053e49eee3783a7c7f42feb42af86c'
 # 初始化机器人小丁
 xiaoding = DingtalkChatbot(webhook, secret=secret)  # 方式二：勾选“加签”选项时使用（v1.5以上新功能）
-
-url = "https://www.aicoin.cn/?long_lives_aicoin=%22live%22"
+url = "https://www.ouyi.cc/priapi/v5/public/markets/important-index?t=1622610543452"
 
 def get_html(urls):
     request = urllib.request
@@ -17,17 +17,18 @@ def get_html(urls):
     html = request.urlopen(req).read().decode()
     return html
 
-
 def parse_html(file):
-    soup = BeautifulSoup(file, 'html.parser')
-    span = soup.find('span', attrs={'class', 'emphasis'})
-    return span.getText()
-
+    soup = BeautifulSoup(file, 'html.parser').getText()
+    json_obj = json.loads(soup)
+    return json_obj["data"][0]["askPx"]
 
 # 每n秒执行一次
 def timer(n):
     while True:
         xiaoding.send_text(msg=parse_html(get_html(url)))
         time.sleep(n)
-# 5s
-timer(15)
+# 8s
+timer(8)
+
+
+
