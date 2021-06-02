e2dfdd384feca88b0c2bc355a930c98ec07b9092
liupengjie <liupengjie123>
2021-06-02 16:54:16 +0800

    dingding btc_pirce push

--- test/ok/test2.py
+++ test/ok/test2.py
@@ -1,6 +1,5 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from threading import Timer
 import json
 import time
 from dingtalkchatbot.chatbot import DingtalkChatbot
@@ -11,14 +10,12 @@ secret = 'SECf50a9ff48267e2669b9402ad8a637f9219053e49eee3783a7c7f42feb42af86c'
 xiaoding = DingtalkChatbot(webhook, secret=secret)  # 方式二：勾选“加签”选项时使用（v1.5以上新功能）
 url = "https://www.ouyi.cc/priapi/v5/public/markets/important-index?t=1622610543452"
 
-
 def get_html(urls):
     request = urllib.request
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
     req = request.Request(url=urls, headers=headers)
     html = request.urlopen(req).read().decode()
     return html
-
 
 def parse_html(file):
     soup = BeautifulSoup(file, 'html.parser').getText()
@@ -30,7 +27,7 @@ def timer(n):
     while True:
         xiaoding.send_text(msg=parse_html(get_html(url)))
         time.sleep(n)
-# 5s
+# 8s
 timer(8)
 
 
