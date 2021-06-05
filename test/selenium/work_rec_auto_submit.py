d9890e868e4c46b56ad25bc269830932975ae45d
liupengjie <lpjmail@163.com>
2021-06-05 08:52:13 +0800

    11

--- test/selenium/work_rec_auto_submit.py
+++ test/selenium/work_rec_auto_submit.py
@@ -1,33 +1,36 @@
-from selenium import webdriver#导入selenium模块
-import time#导入时间模块
+from selenium import webdriver  # 导入selenium模块
+import time  # 导入时间模块
 from selenium.webdriver.support.select import Select
 
 # 工时系统url
-url='http://39.98.72.170:8212/Accounts/SignIn'
+url = 'http://39.98.72.170:8212/Accounts/SignIn'
 # 产品id
-product_id=''
+product_id = ''
 # 用户名
-user_name=''
+user_name = ''
 # 密码
-password=''
+password = ''
 # 浏览器驱动
 driver = webdriver.Chrome()
+
 
 # 登录
 def do_login():
     driver.get(url)
-    input_text=driver.find_element_by_id("USERNAME")
+    input_text = driver.find_element_by_id("USERNAME")
     input_text.send_keys(user_name)
-    input_text=driver.find_element_by_id("INPUT_PASSWORD")
+    input_text = driver.find_element_by_id("INPUT_PASSWORD")
     input_text.send_keys(password)
-    button=driver.find_element_by_class_name("login_btn")
+    button = driver.find_element_by_class_name("login_btn")
     button.click()
+
 
 # 打开工作记录
 def to_work_rec():
-    div=driver.find_element_by_class_name("divTopMenu")
+    div = driver.find_element_by_class_name("divTopMenu")
     a = div.find_elements_by_tag_name("a")[3]
     a.click()
+
 
 # 新增工作
 def add_work_rec():
@@ -46,9 +49,10 @@ def add_work_rec():
     driver.find_element_by_class_name("m-input").click()
     product_lis = driver.find_elements_by_tag_name('li')
     for li in product_lis:
-        if li.get_attribute("data-value")==product_id:
+        if li.get_attribute("data-value") == product_id:
             li.click()
     driver.find_element_by_id('ID_OK').click()
+
 
 do_login()
 time.sleep(3)
---
+++ test/tes.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+for i in range(10000):
+    list_nl = np.random.randint(25, 35, 30)
+    print(list_nl, "平均年龄：", np.mean(list_nl))
+
+
+
