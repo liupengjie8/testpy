3e10e1a4e4de5e1a1a7410af7a555563205d87d3
liupengjie <liupengjie123>
2021-06-30 17:53:42 +0800

    日间

--- test/gyy/index8.py
+++ test/gyy/index8.py
@@ -37,34 +37,40 @@ def get_minutes_diff(str1,str2):
 
 
 def get_mr_ope_result():
-    sql = 'SELECT date,mr_content FROM mrqc_result_first WHERE LEFT(DATE,7) IN (%s)  AND org_id = %s'
-    mycursor.execute(sql, ('2021-01', org_id))
+    sql = 'SELECT date,mr_content,id FROM mrqc_result WHERE LEFT(DATE,7) IN (%s)  AND org_id = %s'
+    mycursor.execute(sql, ('2021-05', org_id))
     datas = mycursor.fetchall()
     day_time_ope =get_daytime_dicts()
     day_ope_count = 0
     for data_row in datas:
         json_obj = json.loads(str(data_row[1]))
-        minutes_idff = get_minutes_diff(json_obj['b12'], json_obj['b15'])
-        if json_obj['b11c'] != 1 and minutes_idff <= 2880:
-            for dic_row in day_time_ope:
-                operations = json_obj['operations']
-                diagnosis = json_obj['diagnosis']
-                flag_ope = False
-                for ope in operations:
-                    if 'c35c' in ope and str(ope['c35c']) in codes_list and str(ope).startswith(dic_row[0]):
-                        flag_ope = True
-                        break
-
-                if flag_ope:
-                    flag_diag = False
-                    for diag in diagnosis:
-                        if str(diag).startswith(dic_row[1]):
-                            flag_diag = True
+        if str(data_row[2]) == '0404b12f-41a5-47bf-91cf-f7a557fd59d4':
+            minutes_idff = get_minutes_diff(json_obj['b12'], json_obj['b15'])
+            if json_obj['b11c'] != 1 and minutes_idff <= 2880:
+                for dic_row in day_time_ope:
+                    operations = json_obj['operations']
+                    diagnosis = json_obj['diagnosis']
+                    flag_ope = False
+                    for ope in operations:
+                        # a = 'c35c' in ope
+                        # b = str(ope['c35c']) in codes_list
+                        # c = str(ope['c35c']).startswith(dic_row[0])
+                        if 'c35c' in ope and str(ope['c35c']) in codes_list and str(ope['c35c']).startswith(dic_row[0]):
+                            flag_ope = True
                             break
 
-                    if flag_diag and flag_ope:
-                        day_ope_count+=1
+                    if flag_ope:
+                        flag_diag = False
+                        for diag in diagnosis:
+                            if 'c06c' in diag and str(diag['c06c']).startswith(dic_row[1]):
+                                flag_diag = True
+                                break
+
+                        if flag_diag and flag_ope:
+                            day_ope_count+=1
     print(day_ope_count)
     print("end~")
 
+
+get_ope_codes()
 get_mr_ope_result()
